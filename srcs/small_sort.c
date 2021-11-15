d5f32e73d52ef975091e05076caecc2fe4611a37
fvarrin <florian.varrin@gmail.com>
2021-11-15 15:35:29 +0100

    Improve sorting algo for three

--- srcs/small_sort.c
+++ srcs/small_sort.c
@@ -6,7 +6,7 @@
 /*   By: fvarrin <florian.varrin@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 14:20:33 by fvarrin           #+#    #+#             */
-/*   Updated: 2021/11/15 15:29:02 by fvarrin          ###   ########.fr       */
+/*   Updated: 2021/11/15 15:32:02 by fvarrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,17 +26,14 @@ void	ft_sort_two(t_stack *stack)
 void	ft_sort_three(t_stack *stack)
 {
 	int		min;
-	int		max;
 
 	min = stack_min(stack);
-	max = stack_max(stack);
 	if (stack_is_sorted(stack))
 		return ;
 	if (min == 2)
 	{
 		ft_rotate_stack(stack);
-		if (max == 0)
-			ft_swap_stack(stack);
+		ft_sort_three(stack);
 	}
 	else
 	{
