750729199aba1fbfbf82fdc5e6d1755fff9b6f54
Florian Varrin <fvarrin@student.42lausanne.ch>
2021-11-27 16:28:05 +0100

    [WIP] improve algo with rotation (not working yet)

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: fvarrin <florian.varrin@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 16:31:19 by fvarrin           #+#    #+#             */
-/*   Updated: 2021/11/27 16:02:00 by fvarrin          ###   ########.fr       */
+/*   Updated: 2021/11/27 16:27:05 by fvarrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	main(int argc, char **argv)
 	t_stack		*stack_b;
 
 	ft_parse_arg(&stack_a, &stack_b, argc, argv);
-	ft_print_stack(stack_a, "Init");
+	/* ft_print_stack(stack_a, "Init"); */
 	if (stack_a->size == 2)
 		ft_sort_two(stack_a);
 	else if (stack_a->size == 3)
@@ -30,7 +30,7 @@ int	main(int argc, char **argv)
 		ft_sort_until_five(stack_a, stack_b);
 	else if (stack_a->size > 5)
 		ft_sort_until_hundred(stack_a, stack_b);
-	ft_print_stack(stack_a, "End");
+	/* ft_print_stack(stack_a, "End"); */
 	ft_destroy_stack(stack_a);
 	ft_destroy_stack(stack_b);
 	return (0);
--- srcs/quick_sort.c
+++ srcs/quick_sort.c
@@ -6,7 +6,7 @@
 /*   By: fvarrin <florian.varrin@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 17:55:46 by fvarrin           #+#    #+#             */
-/*   Updated: 2021/11/27 16:16:50 by fvarrin          ###   ########.fr       */
+/*   Updated: 2021/11/27 16:27:35 by fvarrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,9 +42,9 @@ int	ft_partition(t_stack *stack_a, t_stack *stack_b, int *start, int *end, int *
 	{
 		if (stack_a->arr[i] >= pivot)
 		{
-			printf("Swapping %d and %d\n", ft_get_index(stack_a, boundary + 1, 0), i);
+			/* printf("Swapping %d and %d\n", ft_get_index(stack_a, boundary + 1, 0), i); */
 			rotations = ft_swap_two_number(stack_a, stack_b, ft_get_index(stack_a, ++boundary, 0), i);
-			ft_print_stack(stack_a, "After swap");
+			/* ft_print_stack(stack_a, "After swap"); */
 			i = ft_get_index(stack_a, i, rotations);
 			boundary = ft_get_index(stack_a, boundary, rotations);
 			*start = ft_get_index(stack_a, *start, rotations);
@@ -57,8 +57,8 @@ int	ft_partition(t_stack *stack_a, t_stack *stack_b, int *start, int *end, int *
 		j--;
 	}
 	boundary = ft_get_index(stack_a, boundary + 1, 0);
-	printf("i %d\n", i);
-	printf("boundary %d\n", boundary);
+	/* printf("i %d\n", i); */
+	/* printf("boundary %d\n", boundary); */
 	rotations = ft_swap_two_number(stack_a, stack_b, i, boundary);
 	boundary = ft_get_index(stack_a, boundary, rotations);
 	*start = ft_get_index(stack_a, *start, rotations);
@@ -67,29 +67,28 @@ int	ft_partition(t_stack *stack_a, t_stack *stack_b, int *start, int *end, int *
 	return (boundary);
 }
 
-int	ft_quicksort(t_stack *stack_a, t_stack *stack_b, int start, int end)
+void	ft_quicksort(t_stack *stack_a, t_stack *stack_b, int start, int end, int *rotations)
 {
 	int		boundary;
-	int		rotations;
-	int		left_rotations;
 	
-	printf("quick start %d\n", start);
-	printf("quick end %d\n", end);
+	/* printf("quick start %d\n", start); */
+	/* printf("quick end %d\n", end); */
 	if (start >= end)
-		return (0);
-	rotations = 0;
-	boundary = ft_partition(stack_a, stack_b, &start, &end, &rotations);
+		return ;
+	boundary = ft_partition(stack_a, stack_b, &start, &end, rotations);
 	/* printf("boundary %d\n", boundary); */
-	ft_print_stack(stack_a, "After partition");
+	/* ft_print_stack(stack_a, "After partition"); */
 	/* printf("start %d\n", ft_get_index(stack_a, boundary + 1, 0)); */
 	/* printf("end %d\n", end); */
-	left_rotations = ft_quicksort(stack_a, stack_b, start, ft_get_index(stack_a, boundary - 1, 0));
-	ft_quicksort(stack_a, stack_b, ft_get_index(stack_a, boundary + 1, left_rotations), ft_get_index(stack_a, end, left_rotations));
-	return (rotations);
+	ft_quicksort(stack_a, stack_b, ft_get_index(stack_a, start, *rotations), ft_get_index(stack_a, boundary - 1, 0), rotations);
+	ft_quicksort(stack_a, stack_b, ft_get_index(stack_a, boundary + 1, *rotations), ft_get_index(stack_a, end, *rotations), rotations);
 }
 
 void	ft_sort_until_hundred(t_stack *stack_a, t_stack *stack_b)
 {
-	ft_quicksort(stack_a, stack_b, 0, stack_a->top);
+	int		rotations;
+
+	rotations = 0;
+	ft_quicksort(stack_a, stack_b, 0, stack_a->top, &rotations);
 	ft_rotate_ordered_to_be_sorted(stack_a);
 }
