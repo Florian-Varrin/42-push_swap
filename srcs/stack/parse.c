a9da4b85c1d307227fc6fc8f1db2e0e998e54a4b
Florian Varrin <fvarrin@student.42lausanne.ch>
2022-06-01 15:38:28 +0200

    Fix maxint error

--- srcs/stack/parse.c
+++ srcs/stack/parse.c
@@ -27,7 +27,7 @@ _Bool	check_arg_for_limit(char *arg)
 	length = ft_strlen(arg);
 	if (length > 11)
 		return (false);
-	if (length == 11)
+	if (length == 10 || length == 11)
 	{
 		arg_as_number = ft_atoi(arg);
 		if (arg_as_number > INT_MAX || arg_as_number < INT_MIN)
